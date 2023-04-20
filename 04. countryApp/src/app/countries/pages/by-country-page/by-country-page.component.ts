fa75485b7e8a80066eeed8dc1ac4a57145dbdb8a
Juan David <juandavid918@gmail.com>
2023-04-20 09:53:15 -0500

    Base del proyecto paises app completada

--- 04. countryApp/src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ 04. countryApp/src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { CountriesService } from '../../services/countries.service';
+
+import { Country } from '../../interfaces/country';
+
 @Component({
   selector: 'app-by-country-page',
   templateUrl: './by-country-page.component.html',
@@ -8,4 +12,14 @@ import { Component } from '@angular/core';
 })
 export class ByCountryPageComponent {
 
+  public countries: Country[] = [];
+
+  constructor(private countriesService: CountriesService) {}
+
+  searchByCountry(term: string): void {
+    this.countriesService.searchCountry(term)
+      .subscribe(countries => {
+        this.countries = countries;
+      });
+  }
 }
