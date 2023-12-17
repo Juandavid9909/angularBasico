a2ca5e104afd7ea9ce54d7aa11bef43514b50195
Juan David <juandavid918@gmail.com>
2023-12-17 09:50:17 -0500

    Pipes app en proceso

---
+++ 05. Pipes App/src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,11 @@
+import { MenubarModule } from 'primeng/menubar';
+import { MenuModule } from "primeng/menu";
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  exports: [
+    MenubarModule,
+    MenuModule
+  ]
+})
+export class PrimeNgModule { }
